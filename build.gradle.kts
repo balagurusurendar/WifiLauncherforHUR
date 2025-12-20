774a6064b7fb601946e3a35bf2feb5fa3f72bd8b
balaguru.surendar <balaguru.surendar@zohocorp.com>
2025-12-20 22:01:09 +0530

    gradle version upgraded

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 buildscript {
-    val kotlin_version by extra("1.6.21")
-    val support_lib_version by extra("27.1.1")
     val dokka_version by extra("0.9.16")
 
     repositories {
@@ -9,8 +7,8 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.13.1")
-        classpath("com.google.protobuf:protobuf-gradle-plugin:0.9.5")
+        classpath("com.android.tools.build:gradle:8.13.2")
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.9.6")
         classpath("org.jetbrains.dokka:dokka-android-gradle-plugin:$dokka_version")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
 
@@ -19,7 +17,7 @@ buildscript {
 
 allprojects {
     repositories {
-        jcenter()
+        mavenCentral()
         google()
         maven { url = uri("https://jitpack.io") }
     }
